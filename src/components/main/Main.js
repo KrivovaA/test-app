6786dc86c5b34dab80a40e100bd2f5a1c82d6cd3
KrivovaA <onetwothreesfu@gmail.com>
2018-06-13 00:22:26 +0700

    add styles from @material-ui/core

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles/index';
 
-const Main = () => (
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: 8,
+    paddingBottom: 8,
+    marginTop: theme.spacing.unit,
+  })
+});
+
+const Main = (props) => (
   <div>
-    <p>Please, choose category</p>
+    <Paper className={props.classes.root} elevation={2}>
+      <Typography variant="headline" component="h3">
+        Please, choose category
+      </Typography>
+    </Paper>
   </div>
 );
 
-export default Main;
+export default withStyles(styles)(Main);
